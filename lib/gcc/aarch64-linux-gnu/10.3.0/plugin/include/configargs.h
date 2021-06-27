73f9016a4779b53987a7d8abb6ae686023cd66b0
mcd <mcd@vmbox.localdomain>
2021-06-27 18:25:53 +0200

    tc: Import GCC 10.3.0
    Signed-off-by: mcd <mcd@vmbox.localdomain>

---
+++ lib/gcc/aarch64-linux-gnu/10.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure CFLAGS='-O2 -fstack-protector-strong' CXXFLAGS='-O2 -fstack-protector-strong' --prefix=/home/mcd/android/toolchains/build-tools-gcc/aarch64-linux-gnu --target=aarch64-linux-gnu --disable-multilib --disable-werror --with-bugurl=https://github.com/krasCGQ/build-tools-gcc/issues --with-pkgversion='build-tools-gcc r261 g46d610a3' --enable-languages=c,c++";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
