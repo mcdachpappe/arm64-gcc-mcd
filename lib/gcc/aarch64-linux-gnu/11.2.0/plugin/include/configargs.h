fb2438249b3fdfa839a6ecc148d24a8f83eb1245
mcdachpappe <noreference@web.de>
2021-12-31 12:35:39 +0100

    tc: Import arm64 GCC 11.2.0
    Target: arm64
    Binutils: 2.37
    Builder commit: mcdachpappe/build-tools-gcc@98a7518b28f77e292f4d9c0ae90586ed5dcb78d9
    
    Signed-off-by: mcdachpappe <noreference@web.de>

---
+++ lib/gcc/aarch64-linux-gnu/11.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure CFLAGS='-O2 -fstack-protector-strong' CXXFLAGS='-O2 -fstack-protector-strong' --prefix=/home/mcd/android/toolchains/build-tools-gcc/aarch64-linux-gnu --target=aarch64-linux-gnu --disable-multilib --disable-werror --enable-languages=c,c++";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
